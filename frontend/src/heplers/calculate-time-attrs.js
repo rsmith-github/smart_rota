f54355810cdc5c10b74474e4e4006675c44204ed
rsmith-github <rsmith.git@gmail.com>
2024-01-08 05:51:50 +0000

    add 15 minutes visualizations

--- frontend/src/heplers/calculate-time-attrs.js
+++ frontend/src/heplers/calculate-time-attrs.js
@@ -26,11 +26,18 @@ function calculateTimeAttributes(i, times, element_to_add_minutes, minutes_to_ad
         minutes_to_add = times[3]?.min;
     }
 
-
+    // handle shift start 15/45 minutes
     if (i === times[0]?.hour && times[0]?.min === 45) {
         element_to_add_minutes = i;
         minutes_to_add = 45;
         flip += ' shift-start-45'
+    }
+
+    // handle shift end 15/45 minutes
+    if (i === times[1]?.hour && times[1].min === 15 || i === times[3]?.hour && times[3].min === 15) {
+        element_to_add_minutes = i;
+        minutes_to_add = 15;
+        flip += ' shift-end-45'
     }
 
     return [element_to_add_minutes, minutes_to_add, flip];
