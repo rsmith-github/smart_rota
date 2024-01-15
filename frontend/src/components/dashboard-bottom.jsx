a09f9d308081d15c97dd1d07078b2a33414f00ee
rsmith-github <rsmith.git@gmail.com>
2024-01-15 07:18:07 +0000

    refactor/style: add bottom of dashboard and fix scrolling

---
+++ frontend/src/components/dashboard-bottom.jsx
@@ -0,0 +1,17 @@
+function DashboardBottom() {
+  return (
+    <div className="dashboard-bottom-container">
+      <div id="dashboard-bottom">
+        <div className="dbrd-bottom-quarter"></div>
+        <div className="dbrd-bottom-quarter"></div>
+        <div className="dbrd-bottom-quarter"></div>
+        <div className="dbrd-bottom-quarter no-background">
+          <div className="dbrd-bottom-eigth"></div>
+          <div className="dbrd-bottom-eigth"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default DashboardBottom;
--- frontend/src/pages/dashboard.jsx
+++ frontend/src/pages/dashboard.jsx
@@ -2,6 +2,7 @@ import AppSidebar from "../components/sidebar";
 import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
 import DashboardTop from "../components/dashboard-top";
+import DashboardBottom from "../components/dashboard-bottom";
 
 function Dashboard() {
   const { isAuthenticated } = useSelector((state) => state.user);
@@ -13,8 +14,9 @@ function Dashboard() {
     <div className="page-container">
       <AppSidebar />
 
-      <div className="right-side">
+      <div className="right-side" id="dashboard-container">
         <DashboardTop />
+        <DashboardBottom />
       </div>
     </div>
   );
